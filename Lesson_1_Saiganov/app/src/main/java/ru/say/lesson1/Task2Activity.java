2fef804d9a1894a8f459148e34bade300cad005f
Say_gun <dm.saygunov@gmail.com>
2019-11-14 23:41:06 +0300

    Update Task2Activity.java

--- Lesson_1_Saiganov/app/src/main/java/ru/say/lesson1/Task2Activity.java
+++ Lesson_1_Saiganov/app/src/main/java/ru/say/lesson1/Task2Activity.java
@@ -3,12 +3,38 @@ package ru.say.lesson1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class Task2Activity extends AppCompatActivity {
+
+
+    public class Student{
+        Integer id;
+        String name;
+        String surname;
+        String grade;
+        Integer birthdayYear;
+    }
+
+    public HashMap<Integer, Student> students = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task2);
+
+        Button showStudent = findViewById(R.id.showStudent);
+        final TextView studentsList = findViewById(R.id.studentsList);
+        final EditText newStudent = findViewById(R.id.newStudent);
+        showStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 }
